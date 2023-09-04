3de03d804bb06e631ba7ba92eadc97229c99c8c1
pokon548 <pokon548@hotmail.com>
2023-09-04 06:21:20 +0000

    Disable autoplay video to avoid conflicts with splash logic

--- src/main/index.ts
+++ src/main/index.ts
@@ -15,7 +15,8 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      autoplayPolicy: 'user-gesture-required'
     }
   })
 
